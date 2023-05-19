ad43c7eb581af55c68f07cbe96ee962c9830270e
hdn <hieudn5293@yahoo.de>
2023-05-19 16:13:36 +0200

    init(level-2): complete

---
+++ src/level-2/insta/profile/profile.ts
@@ -0,0 +1,34 @@
+import { inject } from "aurelia";
+import { SocketService } from "../../../advanced/services/SocketService";
+import { StoreService } from "../../../advanced/services/StoreService";
+import "./profile.scss";
+
+@inject()
+export class Profile {
+  private username = "owling";
+
+  constructor(
+    private socketService: SocketService,
+    private storeService: StoreService,
+  ) {
+    this.socketService.onConnectionEstablished((socketId) => {
+      this.storeService.initThisUser({
+        username: this.username,
+        id: socketId,
+      });
+    });
+  }
+
+  private onKeypress(event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      this.socketService.users.emitChangeUser(this.username);
+    }
+
+    return true;
+  }
+
+  attached() {
+    // const socket = this.socketService.getThisSocket();
+    // /* prettier-ignore */ console.log('>>>> _ >>>> ~ file: profile.ts ~ line 31 ~ socket.id', socket.id)
+  }
+}
